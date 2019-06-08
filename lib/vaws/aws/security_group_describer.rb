0cc87c0393d8c17887deb37516a9dc91d0601a2b
Shota Ito <shota.ito.jp@gmail.com>
2019-06-08 19:43:49 +0900

    Modify processing when use Security Group option

--- lib/vaws/aws/security_group_describer.rb
+++ lib/vaws/aws/security_group_describer.rb
@@ -22,16 +22,20 @@ module Vaws
           group_id   = security_group.group_id
 
           security_group.ip_permissions.each do |ip_permission|
-            ip_permission.from_port.nil? ? from_port = "ALL" : from_port = ip_permission.from_port
+            ip_permission.from_port.nil? ? from_port = "ALL" : from_port = ip_permission.from_port.to_s
             ip_permission.ip_protocol == "-1" ? ip_protocol = "ALL" : ip_protocol = ip_permission.ip_protocol
 
+            ip_permission.user_id_group_pairs.each do |user_id_group_pair|
+              cidr_ip = user_id_group_pair.group_id
+              rows << [group_name, group_id, from_port, cidr_ip, ip_protocol]
+            end
             ip_permission.ip_ranges.each do |ip_range|
               cidr_ip = ip_range.cidr_ip
-              rows << [group_name, group_id, cidr_ip, from_port, ip_protocol]
+              rows << [group_name, group_id, from_port, cidr_ip, ip_protocol]
             end
           end
         end
-        @term_table = Terminal::Table.new :headings => ['Name', 'Id', 'FromCidr', 'ToPort', 'Protocol'], :rows => rows.sort
+        @term_table = Terminal::Table.new :headings => ['Name', 'Id', 'AllowPort', 'AllowNetwork', 'AllowProtocol'], :rows => rows.sort
       end
     end
   end
