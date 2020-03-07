c4f50989f12bb978e9e17c119be80c273f6a845b
Shota Ito <shota.ito.jp@gmail.com>
2020-03-07 11:11:27 +0900

    Change apeearance of Route53

--- lib/vaws/aws/route53_describer.rb
+++ lib/vaws/aws/route53_describer.rb
@@ -17,29 +17,30 @@ module Vaws
         get_hosted_zone
 
         @r53_hosted_zone_ids.each do |zone_id|
-          csv_record   = []
-          record_alias = ''
-          records      = @r53_client.list_resource_record_sets({ hosted_zone_id: "#{zone_id}", max_items: 1000 })
+          record_values = ''
+          records       = @r53_client.list_resource_record_sets({ hosted_zone_id: "#{zone_id}", max_items: 1000 })
           records.resource_record_sets.each do |record_sets|
-            record_name  = record_sets.name
-            record_type  = record_sets.type
-            record_ttl   = record_sets.ttl
-            record_alias = record_sets.alias_target.dns_name if !record_sets.alias_target.nil?
-            record_sets.resource_records.each do |record|
-              csv_record << record.value
+            record_name = record_sets.name
+            record_type = record_sets.type
+            record_ttl  = record_sets.ttl
+            if record_sets.alias_target.nil?
+              record_sets.resource_records.each do |record|
+                record_values << "#{record.value.to_s}\n"
+              end
+            else
+              record_values = record_sets.alias_target.dns_name
             end
-            rows << [record_name, record_type, csv_record, record_ttl, record_alias]
-            csv_record   = []
-            record_alias = ''
+            rows << [record_name, record_type, record_values, record_ttl]
+            record_values = ''
           end
         end
-        @term_table = Terminal::Table.new :headings => ['Fqdn', 'Type', 'Value', 'Ttl', 'Alias'], :rows => rows.sort
+        @term_table = Terminal::Table.new :headings => ['Fqdn', 'Type', 'Value', 'Ttl'], :rows => rows.sort
       end
 
       private
 
       def get_hosted_zone
-        @r53_client.list_hosted_zones({ max_items: 1000 }).hosted_zones.each do |zone|
+        @r53_client.list_hosted_zones({ max_items: 100 }).hosted_zones.each do |zone|
           @r53_hosted_zone_ids << zone.id
         end
       end
